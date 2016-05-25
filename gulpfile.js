7f9c4df5f6e5a9e14dfb10517db6c7757e8942b7
Pavel Bazhenov <Pavel Bazhenov>
2016-05-25 22:11:12 +0300

    html injector & other

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,51 @@
-var gulp = require('gulp'),
-    inlineCss = require('gulp-inline-css');
+var gulp        = require('gulp'),
+    inlineCss   = require('gulp-inline-css'),
+    browserSync = require('browser-sync').create(),
+    sass        = require('gulp-sass'),
+    htmlInjector = require("bs-html-injector");
  
 gulp.task('inline', function() {
-    return gulp.src('in/*.html')
+    return gulp.src('public/*.html')
         .pipe(inlineCss({
                 applyStyleTags: true,
                 applyLinkTags: true,
                 removeStyleTags: true,
                 removeLinkTags: true
         }))
-        .pipe(gulp.dest('build/'));
+        .pipe(gulp.dest('inline-code/'));
 });
 
 
 
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+    browserSync.use(htmlInjector, {
+        files: "public/*.html"
+    });
 
-// Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('scss/*.scss', ['sass']);
+    browserSync.init({
+        server: {
+          baseDir: 'public',
+          directory: true
+        }
+    });
+
+    gulp.watch("assets/scss/*.scss", ['sass']);
+    // gulp.watch("public/*.html").on('change', browserSync.reload);
 });
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("assets/scss/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("public/css"))
+        .pipe(browserSync.stream());
+});
+
+
 
 // Default Task
-gulp.task('default', ['watch']);
+
+gulp.task("watch", ["serve"], function () {
+    gulp.watch("test/fixtures/*.html", htmlInjector);
+});
